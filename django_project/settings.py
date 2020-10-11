35fa83d35b886182a65e34e964e85e08b8ad8a61
gavole <bethwelifedha@gmail.com>
2020-10-11 14:35:45 +0300

    updated file

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-721aas7-h&eqyrv8bw^(3^707cl0j=%6kfqz#(@k%b=#%=bkm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
